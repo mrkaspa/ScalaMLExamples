0082366cacf37d12ff6d77b88fb4402c198314d1
Michel Perez <michel.ingesoft@gmail.com>
2015-06-17 21:43:53 -0500

    adding regularization to logistic regresion

---
+++ src/main/scala/com/mrkaspa/ml/ex2/LogisticRegressionWithRegularization.scala
@@ -0,0 +1,66 @@
+package com.mrkaspa.ml.ex2
+
+import java.io.File
+
+import breeze.linalg._
+import breeze.numerics.log
+import breeze.optimize.{DiffFunction, LBFGS}
+
+/**
+ * Created by michelperez on 6/15/15.
+ */
+object LogisticRegressionWithRegularization extends App {
+
+  val file = new File(getClass.getResource("/ex2data1.txt").getFile)
+  val csv = csvread(file)
+  val X = csv(::, 0 to 1)
+  val y = csv(::, 2).asDenseMatrix.t
+  val lambda = 1d
+
+  //concatenates a col [1...1] to the features matrix
+  val X1 = DenseMatrix.horzcat(DenseMatrix.ones[Double](X.rows, 1), X)
+
+  //function used in logistic regression
+  def sigmoid(m: DenseMatrix[Double]): DenseMatrix[Double] = {
+    m.map { value =>
+      1d / (1d + scala.math.exp(-value))
+    }
+  }
+
+  val f = new DiffFunction[DenseVector[Double]] {
+
+    //calculates J and theta
+    def calculate(theta: DenseVector[Double]) = {
+      val m = y.rows
+      //the hypotesis here changes for logistic regression
+      val h = sigmoid(X1 * theta.asDenseMatrix.t)
+      val sqrErrors = (h - y) :^ 2d
+      val costPos = (-y.t) * log(h)
+      val costNeg = (1d - y.t) * log(1d - h)
+      val Jm = 1d / m * (costPos - costNeg)
+      val grad: DenseMatrix[Double] = (1d / m) * (X1.t * (h - y))
+      val J: Double = Jm(0, 0)
+      //adding the regularization
+      theta(0) = 0
+      val JReg = J + (lambda / 2 * m) * (theta.t * theta)
+      val gradReg: DenseMatrix[Double] = grad + ((lambda / m) * theta).asDenseMatrix.t
+      (JReg, gradReg.toDenseVector)
+    }
+  }
+
+  val lbfgs = new LBFGS[DenseVector[Double]](maxIter = 1000, m = 5)
+
+  val initialTheta = DenseVector(0.0, 0.0, 0.0)
+
+  println()
+
+  //minimize theta using the LBFGS algorithm
+  val newTheta = lbfgs.minimize(f, initialTheta)
+
+  println(s"newTheta >> $newTheta")
+
+  val prob = sigmoid((DenseMatrix(1.0, 45.0, 85.0).t * newTheta).asDenseMatrix);
+
+  println(s"prob >> $prob")
+
+}
